ea09efa59fdacb98e826cee6e3da899818e742e6
AndersonFornalski <claian@live.com>
2019-06-24 14:32:20 -0300

    criado classe que inclui dados no DataBase do H2

---
+++ src/main/java/com/Fornalski/cursoFornalski/CursoFornalskiApplication.java
@@ -0,0 +1,13 @@
+package com.Fornalski.cursoFornalski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFornalskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFornalskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fornalski/cursoFornalski/CursoFornalskiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Fornalski.cursoFornalski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoFornalskiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
